6a5e09818f32119f3d54a4d693ee048ea0f872d0
dallas-vanwyk <dallas.vanwyk@gmail.com>
2025-05-10 15:26:10 -0500

    completed main exercises

--- exercises.py
+++ exercises.py
@@ -122,9 +122,10 @@ print('Exercise 4:', hometown_info())
 # Use a for loop to iterate over the key: value pairs in the home_town dictionary and append a string with the following format to home_town_items: "<key> = <value>"
 
 def list_home_town_items():
-    pass
-
-    # your code here
+    home_town_items = []
+    for key, value in home_town.items():
+        home_town_items.append(f"{key} = {value}")
+    return home_town_items
 
 # Call the function and print the result
 print('Exercise 5:', list_home_town_items())
